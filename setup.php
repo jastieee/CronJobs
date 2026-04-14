e23408aa3d39d7069201e13d1b640cd3d022e92a
jastieee <cruzjastie@gmail.com>
2026-04-14 12:03:25 +0800

    NoDB

--- setup.php
+++ setup.php
@@ -4,23 +4,21 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $DB_FULL_NAME = 'ecandycom_ecandy_db';
-$DB_FULL_USER = 'ecandycom_dbuser';
-$DB_PASS      = 'NSSI@2026!';
+$DB_FULL_USER = 'ecandycom_3e76nusq098u';
+$DB_PASS      = '5a75273fcb5103e147620b0f2bf9e03d79c62848';
 
 echo "<h2>Testing Connection...</h2>";
 
-// Connect WITHOUT database first
-$conn = mysqli_connect('localhost', $DB_FULL_USER, $DB_PASS);
+$conn = mysqli_connect('localhost', $DB_FULL_USER, $DB_PASS, $DB_FULL_NAME);
 
 if (!$conn) {
     die("<p style='color:red'>❌ Error " . mysqli_connect_errno() . ": " . mysqli_connect_error() . "</p>");
 }
 
-echo "<p style='color:green'>✅ User connected!</p>";
+echo "<p style='color:green'>✅ Connected successfully!</p>";
 
-// Show all databases this user can see
-$result = mysqli_query($conn, "SHOW DATABASES");
-echo "<h3>Databases visible to this user:</h3><ul>";
+$result = mysqli_query($conn, "SHOW TABLES");
+echo "<h2>Tables:</h2><ul>";
 while ($row = mysqli_fetch_row($result)) {
     echo "<li>" . $row[0] . "</li>";
 }
